9c9d827fec1702f97c955432c554ac71901df0e8
Marc Yang <m.y.yang001@gmail.com>
2016-04-09 14:05:31 -0400

    random commit

---
+++ owl-carousel/custom.js
@@ -0,0 +1,16 @@
+
+
+    $(document).ready(function() {
+     
+      $("#owl-demo").owlCarousel({
+     
+          autoPlay: 3000, //Set AutoPlay to 3 seconds
+     
+          items : 4,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3]
+     
+      });
+     
+    });
+

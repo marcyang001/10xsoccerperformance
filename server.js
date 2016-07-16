0bacf436cf90977f6f8f4869db8e19daf635c05c
Marc Yang <m.y.yang001@gmail.com>
2016-07-16 15:15:58 -0400

    adjust a bit

--- server.js
+++ server.js
@@ -160,7 +160,9 @@ app.post('/process_post', urlencodedParser, function (req, res, next) {
 if (module === require.main) {
   // [START server]
   // Start the server
-  	var server = app.listen(process.env.PORT || 8080, function () {
+    var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+    var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+  	var server = app.listen(server_port, server_ip_address, function () {
     var host = server.address().address;
     var port = server.address().port;
 
